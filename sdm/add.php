ed8901e316b669f4caf70ec6d628a5b08ded4d56
agussyabany <agussyabany@gmail.com>
2021-02-15 17:36:55 +0800

    membuat fungsi add

--- sdm/add.php
+++ sdm/add.php
@@ -27,37 +27,47 @@
               </div>
               <!-- /.card-header -->
               <!-- form start -->
-              <form class="form-horizontal">
+              <form method="post" action="proses.php" class="form-horizontal">
                 <div class="card-body">
 
                   <div class="form-group row">
                     <label for="inputPassword3" class="col-sm-2 col-form-label">Nama</label>
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="inputPassword3" placeholder="Nama SDM Pendukung" required>
+                      <input name="nama" type="text" class="form-control" id="inputPassword3" placeholder="Nama SDM Pendukung" required>
                     </div>
                   </div>
                   
-                  <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label" required>Jabatan</label>
+                <div class="form-group row">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label" required>Jabatan</label>
                     <div class="col-sm-6">
                       <!-- select -->
-                      <div class="form-group">
-                        <select class="form-control js-example-basic-single">
-                          <option value="option1">option 1</option>
-                          <option value="option2">option 2</option>
-                          <option value="option3">option 3</option>
-                          <option value="option4">option 4</option>
-                          <option value="option5">option 5</option>
-                        </select>
 
+                      <div class="form-group">
+                        <select name="jabatan" id="jabatan" class="form-control js-example-basic-single">
+                          <option value="1">Menejer</option>
+                          <option value="2">Asisten Menejer</option>
+                          <option value="2">Staff</option>
+                        </select>
+                      </div>
+
+                      
+                      <div class="form-group">
+                        <select name="divdep" id="divdep"  class="form-control js-example-basic-single">
+
+                        </select>
+                      </div>
+
+                      <div class="form-group">
                         <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio1">
+                          <input class="form-check-input" type="radio" name="status" id="status" value="aktiv">
                           <label class="form-check-label">Aktif</label>
                         </div>
+
                         <div class="form-check">
-                          <input class="form-check-input" type="radio" name="radio1" checked="">
+                          <input class="form-check-input" type="radio" name="status" id="status" checked="" value="tidak aktiv">
                           <label class="form-check-label">Tidak Aktif</label>
                         </div>
+
                       </div>
                     </div>
                   </div>
@@ -68,7 +78,7 @@
                 <!-- /.card-body -->
                 <div class="card-footer">
                   <button type="submit" class="btn btn-primary">KEMBALI</button>
-                  <button type="submit" class="btn btn-primary float-right">SUBMIT</button>
+                  <button type="submit" name="add" class="btn btn-primary float-right">SUBMIT</button>
                 </div>
                 <!-- /.card-footer -->
               </form>
@@ -76,16 +86,45 @@
             </div>
             </div>
           </div>
-            </section>
+      </section>
             </div>
             </div>
 </div>
 
 
 <script>
-  $(document).ready(function() {
-    $('.js-example-basic-single').select2();
-    });
+
+  $("#jabatan").change(function(){
+            // variabel dari nilai combo box kendaraan
+            var Jabatan = $("#jabatan").val();
+
+            // Menggunakan ajax untuk mengirim dan dan menerima data dari server
+            $.ajax({
+                type: "POST",
+                dataType: "html",
+                url: "pilih-data.php",
+                data: "jabatan="+Jabatan,
+                success: function(data){
+                   $("#divdep").html(data);
+                }
+            });
+        });
+
+        // $("#divdep").change(function(){
+        //     // variabel dari nilai combo box merk
+        //     var DivDep = $("#divdep").val();
+
+        //     // Menggunakan ajax untuk mengirim dan dan menerima data dari server
+        //     $.ajax({
+        //         type: "POST",
+        //         dataType: "html",
+        //         url: "pilih-data.php",
+        //         data: "divdep="+DivDep,
+        //     //     success: function(data){
+        //     //         $("#tipe").html(data);
+        //     //     }
+        //     });
+        // });
 
 
 </script>
